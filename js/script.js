0674403821fc43c7ee064bcb83d6e936696508ea
johndoenma <johndoenma@gmail.com>
2020-06-02 10:45:50 -1000

    added swup js options

--- js/script.js
+++ js/script.js
@@ -1,12 +1,40 @@
-// NAV
-const mysitenav = document.querySelector('.site-header .site-nav');
-const mymenubutton = document.querySelector('.menu-button');
-mymenubutton.onclick = function () {
-    mysitenav.classList.toggle('active');
-}
 
-const swup = new Swup();
 
-// swup.on('animationOutDone', function () {
-//     console.log("leave is done");
-// });
+document.addEventListener("DOMContentLoaded", function () {
+
+    // NAV
+    const mysitenav = document.querySelector('.site-header .site-nav');
+    const mymenubutton = document.querySelector('.menu-button');
+    mymenubutton.onclick = function () {
+        mysitenav.classList.toggle('active');
+    }
+
+    // SWUP: enable swup
+    let options = {
+        LINK_SELECTOR: 'a[href^="' + window.location.origin + '"]:not([data-no-swup]), a[href^="./"]:not([data-no-swup]), a[href^="#"]:not([data-no-swup])',
+        FORM_SELECTOR: 'form[data-swup-form]',
+        elements: [
+            '#swup'
+        ],
+        animationSelector: '[class*="transition-"]',
+        cache: true,
+        pageClassPrefix: '',
+        scroll: true,
+        debugMode: true,
+        preload: true,
+        support: true,
+        skipPopStateHandling: function (event) {
+            if (event.state && event.state.source == "swup") {
+                return false;
+            }
+            return true;
+        },
+        animateHistoryBrowsing: false,
+    }
+    const swup = new Swup(options);
+
+    swup.on('animationOutDone', function () {
+        console.log("leave is done");
+    });
+
+});
