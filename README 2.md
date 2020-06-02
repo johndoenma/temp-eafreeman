7a47bd868f2b9d670f58b5b01a44761e0d70927c
kccnma <gargiulo@hawaii.edu>
2020-06-01 17:45:49 -1000

    updated to latest version

---
+++ README 2.md
@@ -0,0 +1,3 @@
+# eafreeman-portfolio
+
+View [Github Pages Demo](https://johndoenma.github.io/temp-eafreeman)
---
+++ js/slick 2.js
@@ -0,0 +1,23 @@
+// SLICK SLIDER from https://kenwheeler.github.io/slick/ //
+
+
+// initialize slick
+$('.single-item').slick({
+    arrows: false,
+    autoplay: true,
+    dots: false,
+    centerMode: false,
+    slidesToShow: 1,
+    lazyLoad: 'progressive',
+    speed: 2000,
+});
+
+// next button
+$('.next-btn').click(function () {
+    $('.single-item').slick('slickNext');
+});
+
+// previous button
+$('.prev-btn').click(function () {
+    $('.single-item').slick('slickPrev');
+});
